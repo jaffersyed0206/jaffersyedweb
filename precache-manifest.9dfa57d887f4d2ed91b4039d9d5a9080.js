ddc125fbdcbdd3305f4633c8aed2850870bf1205
Jaffer Syed <JafferSyed@USF-Gold-Wifi-226-57-7.laptops.usf.edu>
2020-01-22 15:42:55 -0500

    Updates

---
+++ precache-manifest.9dfa57d887f4d2ed91b4039d9d5a9080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889f4718cedd8bcf215360b814e6a376",
+    "url": "/jaffersyedweb/index.html"
+  },
+  {
+    "revision": "a55362d5bdb40f880609",
+    "url": "/jaffersyedweb/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6487ee93326a333ae992",
+    "url": "/jaffersyedweb/static/css/main.b7446e9b.chunk.css"
+  },
+  {
+    "revision": "a55362d5bdb40f880609",
+    "url": "/jaffersyedweb/static/js/2.36d72854.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jaffersyedweb/static/js/2.36d72854.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6487ee93326a333ae992",
+    "url": "/jaffersyedweb/static/js/main.e66c77fe.chunk.js"
+  },
+  {
+    "revision": "947b397f9a71d5099b46",
+    "url": "/jaffersyedweb/static/js/runtime-main.a57faeb8.js"
+  }
+]);
